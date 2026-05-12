84bd623d9b379fde5d01067c018751b5c8bcb2cc
valmirasuka <valmirasuka2@gmail.com>
2026-05-12 15:22:25 +0200

    Quiz solutions

---
+++ testi.js
@@ -0,0 +1,163 @@
+// 1. returnOnlyOddPeople(people)
+// Shkruani një funksion që pranon një listë personash, ku secili person ka një ID, dhe kthen vetëm ata që kanë ID tek (odd number).
+// const people = [
+// { id: 1, name: "Arta" },
+// { id: 2, name: "Blerim" },
+// { id: 3, name: "Doni" },
+// { id: 4, name: "Elira" }
+// ];
+// Shpjegimi:
+// Numrat tek janë ata që kur i pjestojmë me 2, mbetja (remainder) është 1.
+// Kështu kontrollojmë id % 2 !== 0.
+
+// 2. maxNr(a, b)
+// Shkruani një funksion maxNr(a, b) që kthen numrin më të madh mes dy numrave.
+
+// 3. isLandscape(width, height)
+// Shkruani një funksion që kthen true nëse gjerësia është më e madhe se lartësia.
+// Shpjegimi:
+// Nëse width është më i madh se height, fotoja është në pozicion landscape.
+
+// 4. fizzBuzz(input)
+// Shkruani funksionin fizzBuzz që bën këto gjëra:
+// Nëse numri pjestohen me 3 → “Fizz”
+// Me 5 → “Buzz”
+// Me 3 dhe 5 → “FizzBuzz”
+// Nëse nuk është numër → “not a number”
+// remainder me 3 kur osht mbetja 0 
+
+// 5. checkSpeed(speed)
+// Shkruani funksionin checkSpeed që:
+// nëse speed ≤ 70 → “Ok”
+// çdo 5 km/h mbi limit → 1 pikë
+// nëse pikët ≥ 12 → “License suspended”
+
+// Shpjegimi:
+// Çdo 5 km/h mbi 70 jep 1 pikë; nëse pikët arrijnë 12, shoferi humb patentën.
+// Bonus: Math.floor() -> përdoret për te rrumbullaksuar nje numer dhjetor
+
+// 6. countTruthy(array)
+// Shkruani funksion që numëron sa elemente të array-it janë “truthy”.
+// 📌 Shembull array:
+// [0, null, undefined, 2, 3] → rezultati duhet të jetë 2
+// Shpjegimi:
+// Truthy janë vlerat që JavaScript i konsideron të vërteta, si numrat pozitivë, stringjet jo bosh, etj
+
+// 7. sum(limit)
+// Shkruani një funksion që merr një array me numra dhe gjen shumën e të gjithë numrave që pjestohen me 3 ose me 5.
+// const numbers = [1, 3, 5, 6, 7, 9, 10];
+// Shpjegimi:
+// Shikojmë çdo numër tek array-i. Nëse një numër pjestohen me 3 ose me 5, e shtojmë në shuma. Në fund kthejmë shumën.
+
+// 8. Krijo një listë notash [9, 8, 5, 10] dhe gjej mesataren duke përdorur forEach.
+
+// 9. Krijo një funksion getNumbersGreaterThan10(numbers) që kthen vetëm numrat më të mëdhenj se 10. me filter
+
+// 10. Krijo një funksion formatProductNames(products) që kthen një array të ri ku çdo emër ka tekstin:
+// "Product: " përpara emrit.
+
+// Psh:
+// ["Product: Laptop", "Product: Mouse", "Product: Keyboard"] filter
+
+const people = [
+    { id: 1, name: "Arta" },
+    { id: 2, name: "Blerim" },
+    { id: 3, name: "Doni" },
+    { id: 4, name: "Elira" }
+];
+
+const returnOnlyOddPeople = people => people.filter(oddPeople => oddPeople.id % 2 !== 0)
+
+console.log(returnOnlyOddPeople(people))
+
+const maxNr = (a, b) => {
+    if (a > b) {
+        console.log(a)
+    } else {
+        console.log(b)
+    }
+}
+
+maxNr(10, 5)
+
+const isLandscape = (width, height) => console.log(`Landscape mode: ${width > height}`)
+
+isLandscape(40, 20)
+
+const fizzBuzz = input => {
+    if (input % 3 === 0 && input % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (input % 5 === 0) {
+        console.log('Buzz')
+    } else if (input % 3 === 0) {
+        console.log('Fizz')
+    } else {
+        console.log('not a number')
+    }
+}
+
+fizzBuzz(12)
+
+const checkSpeed = speed => {
+    if (speed <= 70) {
+        console.log('Ok');
+        return;
+    }
+
+    const piket = Math.floor((speed - 70) / 5);
+
+    if (piket >= 12) {
+        console.log('License suspended');
+    } else {
+        console.log('Piket:', piket);
+    }
+}
+
+checkSpeed(10)   
+checkSpeed(80)  
+checkSpeed(130)  
+
+let elements = [0, null, undefined, 2, 3, 'Valmira'];
+
+const countTruthy = array => array.filter(element => element).length
+
+console.log(countTruthy(elements));
+
+const numbers = [1, 3, 5, 6, 7, 9, 15];
+
+let sum1 = 0;
+
+const sum = limit => {
+    limit.forEach(element => {
+        if (element % 3 === 0 && element % 5 === 0) {
+            sum1 = sum1 + element
+        }
+    });
+}
+
+sum(numbers)
+console.log(sum1)
+
+const notat = [9, 8, 5, 10]
+
+let mesatarja = 0
+let shuma = 0
+
+notat.forEach(element => {
+    shuma = shuma + element
+    mesatarja = shuma / 4 
+})
+
+console.log(mesatarja)
+
+const numbers1 = [90, 8, 50, 10, 5]
+
+const getNumbersGreaterThan10 = numbers => numbers.filter(element => element > 10)
+
+console.log(getNumbersGreaterThan10(numbers1))
+
+let products = ["Laptop", "Mouse", "Keyboard"]
+
+const formatProductNames = products => products.map(product => "Product: " + product)
+
+console.log(formatProductNames(products))
---
+++ testi.md
@@ -0,0 +1,108 @@
+🟦 1) FUNCTIONS – Pyetje të Numeruara
+
+Çfarë është një funksion?
+Pse përdoren funksionet?
+Cili është dallimi mes parameters dhe arguments?
+Çfarë kthen një funksion nëse nuk përdor return?
+Çfarë është një function expression?
+Shkruaje nje arrow function
+
+🟩 2) OBJECTS – Pyetje të Numeruara
+
+Çfarë është një objekt në JavaScript?
+Si aksesohen vlerat e një objekti?
+Çfarë është një method?
+A mund të kemi arrays brenda objekteve?
+Cila është diferenca mes {} dhe null?
+
+🟧 3) ARRAYS – Pyetje të Numeruara
+
+Çfarë është një array?
+Nga fillon indeksi i një array?
+Çfarë bën push()?
+Çfarë bën pop()?
+A mund të kombinojmë dy arrays?
+Çfarë është një nested array?
+
+🟥 4) CONDITIONS & LOOPS – Pyetje të Numeruara
+Kur përdorim if / else?
+Kur përdoret switch?
+Kur përdoret for loop?
+Kur përdoret while loop?
+Cili është ndryshimi midis while dhe do...while?
+Çfarë bën forEach()?
+Cka eshte .map, .filter dhe dalllimi mes tyre
+
+Çfarë është një funksion?
+Nje bllok i kodit qe kryen nje pune dhe mundemi me riperdore 
+
+Pse përdoren funksionet?
+per mos me perserit kod 
+
+Cili është dallimi mes parameters dhe arguments?
+parametrat jane variablat qe vendosen te deklarimi
+argumentet jane vlerat qe i pasohen funksionit kur thirret
+
+Çfarë kthen një funksion nëse nuk përdor return?
+undefined 
+
+Çfarë është një function expression?
+funksion qe vlera qe e kthen ruhet ne nje variabel
+
+Shkruaje nje arrow function
+const arrowFunction = () => 'Valmira'
+
+Çfarë është një objekt në JavaScript?
+strukture qe perdoret per te ruajtur te dhenat si key value
+
+Si aksesohen vlerat e një objekti?
+me emrin e properties shembull object.name
+
+Çfarë është një method?
+funksionet qe deklarohen brenda objektit
+
+A mund të kemi arrays brenda objekteve?
+po mund te kemi 
+
+Cila është diferenca mes {} dhe null?
+{} objekt empty ndersa null mungese vlere
+
+Çfarë është një array?
+strukture qe ndihmone me rujt te dhena te tipeve te njejta ose te ndryshme
+
+Nga fillon indeksi i një array?
+nga 0 
+
+Çfarë bën push()?
+shton elemente ne fund te arrayit 
+
+Çfarë bën pop()?
+per te fshire elementin e fundit te arrayit
+
+A mund të kombinojmë dy arrays?
+po me metoden concat()
+
+Çfarë është një nested array?
+array brenda arrayit
+
+Kur përdorim if / else?
+kur deshirojme me kry nje pune me nje kusht
+
+Kur përdoret switch?
+kur kemi shume opsione dhe if nuk shihet si opsion per shkak te mos kuptimit te kodit
+
+Kur përdoret for loop?
+kur deshirojme te perserisim nje bllok kodi disa here
+
+Kur përdoret while loop?
+kur deshirojme te perserisim nje bllok kodi me nje kusht
+
+Cili është ndryshimi midis while dhe do...while?
+do while e ekzekuton kodin brenda do se pari pastaj e shikon kushtin, perkunder while qe me fillim e kqyr kushtin
+
+Çfarë bën forEach()?
+iteron ne vlera te arrayit, permban funksion qe na jep elementin indexin dhe arrayin 
+
+Cka eshte .map, .filter dhe dalllimi mes tyre
+2 metoda te arrayit, map transformon elementet ne nje array te ri
+filter - kur deshirojm me filtru elemente sipas nje kushti
